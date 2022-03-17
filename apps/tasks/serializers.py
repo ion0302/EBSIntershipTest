6ab69c42d2cd83aac47a20466b50b1fc86148ee4
Bunduchi  Ion <ibunduche@gmail.com>
2022-03-17 17:50:47 +0200

    M2 6

--- apps/tasks/serializers.py
+++ apps/tasks/serializers.py
@@ -6,5 +6,16 @@ from apps.tasks.models import Task
 class TaskSerializer(ModelSerializer):
     class Meta:
         model = Task
+        fields = '__all__'
+
+
+class TaskPostSerializer(ModelSerializer):
+    class Meta:
+        model = Task
         fields = ['title', 'description', 'status']
 
+
+class TaskListSerializer(ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['id', 'title']
--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -2,8 +2,9 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
+from apps.tasks import serializers
 from apps.tasks.models import Task
-from apps.tasks.serializers import TaskSerializer
+from apps.tasks.serializers import TaskSerializer, TaskPostSerializer
 
 
 class TaskViewSet(ModelViewSet):
@@ -14,6 +15,13 @@ class TaskViewSet(ModelViewSet):
         SearchFilter,
         OrderingFilter
     ]
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return TaskPostSerializer
+        if self.action == 'list':
+            return serializers.TaskListSerializer
+        return serializers.TaskSerializer
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)

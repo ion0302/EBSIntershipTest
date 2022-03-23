37932fddf5c208194b549c6573688429c79c6d06
Bunduchi  Ion <ibunduche@gmail.com>
2022-03-23 16:47:23 +0200

    Update "Add time log"

--- apps/logs/serializers.py
+++ apps/logs/serializers.py
@@ -1,4 +1,7 @@
+from abc import ABC
+
 from django.db import models
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from apps.logs.models import Log
@@ -6,12 +9,26 @@ from apps.logs.models import Log
 
 class LogSerializer(ModelSerializer):
 
+    class Meta:
+        model = Log
+        fields = '__all__'
+        extra_kwargs = {
+            'user': {'read_only': True},
+            'stop': {'read_only': True},
+
+        }
+
+
+class LogPostSerializer(ModelSerializer):
+
+    duration = serializers.DurationField()
 
     # def create(self, validated_data):
     #     validated_data['user'] = self.context['request'].user
     #     validated_data['stop'] = validated_data['start'] + validated_data['duration']
-    #     instance = super(LogSerializer, self).create(validated_data)
-    #
+    #     instance = LogSerializer.create()
+    #     task = instance.task
+    #     task.work_time = task.work_time + validated_data['duration']
     #     return instance
 
     class Meta:
@@ -20,4 +37,6 @@ class LogSerializer(ModelSerializer):
         extra_kwargs = {
             'user': {'read_only': True},
             'stop': {'read_only': True},
+            'duration': {'write_only': True},
         }
+
--- apps/logs/views.py
+++ apps/logs/views.py
@@ -5,8 +5,9 @@ from rest_framework.serializers import Serializer
 from rest_framework.viewsets import ModelViewSet
 import datetime
 
+from apps.logs import serializers
 from apps.logs.models import Log
-from apps.logs.serializers import LogSerializer
+from apps.logs.serializers import LogSerializer, LogPostSerializer
 
 
 class LogViewSet(ModelViewSet):
@@ -14,4 +15,23 @@ class LogViewSet(ModelViewSet):
     queryset = Log.objects.all()
     permission_classes = [IsAuthenticated]
 
+    # def get_serializer_class(self):
+    #     if self.action == 'create':
+    #         return LogPostSerializer
+    #     return LogSerializer
 
+    @action(detail=False, methods=['POST'], serializer_class=LogPostSerializer, url_path='add-log')
+    def add_log(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=self.request.data)
+        serializer.is_valid(raise_exception=True)
+
+        duration = serializer.validated_data['duration']
+        start = serializer.validated_data['start']
+        stop = duration + start
+        task = serializer.validated_data['task']
+        log = Log.objects.create(user=self.request.user, stop=stop, task=task, start=start)
+
+        task.work_time += duration
+        task.save()
+
+        return Response(LogSerializer(log).data)

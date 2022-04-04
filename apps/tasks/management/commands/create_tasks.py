88e85a6e13ebd7cc44d78cfe4571579df0dcf2c4
Bunduchi  Ion <ibunduche@gmail.com>
2022-04-04 18:26:26 +0300

    Create a script that will add random tasks and timelogs

---
+++ apps/tasks/management/commands/create_tasks.py
@@ -0,0 +1,26 @@
+import random
+
+from django.contrib.auth.models import User
+
+from django.core.management.base import BaseCommand
+from django.utils.crypto import get_random_string
+
+from apps.tasks.models import Task
+
+
+class Command(BaseCommand):
+    help = 'Create random tasks'
+
+    def add_arguments(self, parser):
+        parser.add_argument('total', type=int, help='Indicates the number of tasks to be created')
+
+    def handle(self, *args, **kwargs):
+        total = kwargs['total']
+        users_id = list(User.objects.values_list('id', flat=True))
+        for i in range(total):
+            Task.objects.create(title=get_random_string(length=15),
+                                description=get_random_string(length=200),
+                                created_by_id=random.choice(users_id),
+                                assigned_to_id=random.choice(users_id),
+                                is_completed=random.choice([True, False]))
+        self.stdout.write(self.style.SUCCESS('Successfully added tasks'))
---
+++ apps/tasks/management/commands/create_timelogs.py
@@ -0,0 +1,28 @@
+import random
+from datetime import timedelta
+
+from django.contrib.auth.models import User
+
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from django.utils.crypto import get_random_string
+
+from apps.tasks.models import Task, TimeLog
+
+
+class Command(BaseCommand):
+    help = 'Create random timelogs'
+
+    def add_arguments(self, parser):
+        parser.add_argument('total', type=int, help='Indicates the number of timelogs to be created')
+
+    def handle(self, *args, **kwargs):
+        total = kwargs['total']
+        users_id = list(User.objects.values_list('id', flat=True))
+        tasks_id = list(Task.objects.values_list('id', flat=True))
+        for i in range(total):
+            TimeLog.objects.create(started_at=timezone.now() - timedelta(hours=i),
+                                   duration=timedelta(minutes=random.randint(20, 1000)),
+                                   task_id=random.choice(tasks_id),
+                                   user_id=random.choice(users_id))
+        self.stdout.write(self.style.SUCCESS('Successfully added timelogs'))
